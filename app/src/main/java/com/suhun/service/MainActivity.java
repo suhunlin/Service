a700c90b7e7048563f618332ae2474e3a13a5c66
suhunlin <lin.suhun@msa.hinet.net>
2023-09-20 16:30:06 +0800

    Add ActivityResultLauncher to go new Activity

--- app/src/main/java/com/suhun/service/MainActivity.java
+++ app/src/main/java/com/suhun/service/MainActivity.java
@@ -1,11 +1,27 @@
 package com.suhun.service;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goStartServiceFun(View view){
-
+        Intent intent = new Intent(this, StartServiceActivity.class);
+        resultLauncher.launch(intent);
     }
 
     public void goBindServiceFun(View view){
-
+        Intent intent = new Intent(this, BindServiceActivity.class);
+        resultLauncher.launch(intent);
     }
 }
