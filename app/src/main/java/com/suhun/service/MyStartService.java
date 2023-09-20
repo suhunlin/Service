8ab5e060c2bb879491374c3b6dbed3b20ea561b1
suhunlin <lin.suhun@msa.hinet.net>
2023-09-20 18:18:18 +0800

    Implement press start to add timer and timer task to add counter;

--- app/src/main/java/com/suhun/service/MyStartService.java
+++ app/src/main/java/com/suhun/service/MyStartService.java
@@ -5,8 +5,27 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyStartService extends Service {
     private String tag = MyStartService.class.getSimpleName();
+    private Timer timer;
+    private int counter, maxValue;
+    private class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            if(counter < maxValue){
+                counter++;
+                Log.d(tag, "-----#####MyStartService counter #####-----"+counter);
+                Intent intent = new Intent("suhun");
+                intent.putExtra("counterValue", counter);
+                sendBroadcast(intent);
+            }else {
+                cancel();
+            }
+        }
+    }
     public MyStartService() {
     }
 
@@ -20,11 +39,22 @@ public class MyStartService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(tag, "-----#####MyStartService onCreate #####-----");
+        timer = new Timer();
+        counter = 0;
+        maxValue = 100;
+        Intent intent = new Intent("suhun");
+        intent.putExtra("maxCountValue", maxValue);
+        sendBroadcast(intent);
+        timer.schedule(new MyTask(), 100, 100);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(tag, "-----#####MyStartService onStartCommand #####-----");
+        int userChange = intent.getIntExtra("userChange", -1);
+        if(userChange>0){
+            counter = userChange;
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -32,5 +62,14 @@ public class MyStartService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(tag, "-----#####MyStartService onDestroy #####-----");
+        if(timer!=null){
+            timer.cancel();
+            timer.purge();
+            timer =null;
+            counter = 0;
+            Intent intent = new Intent("suhun");
+            intent.putExtra("setCounterZero", 0);
+            sendBroadcast(intent);
+        }
     }
 }
--- app/src/main/java/com/suhun/service/StartServiceActivity.java
+++ app/src/main/java/com/suhun/service/StartServiceActivity.java
@@ -2,7 +2,10 @@ package com.suhun.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
@@ -12,6 +15,28 @@ public class StartServiceActivity extends AppCompatActivity {
     private String tag = StartServiceActivity.class.getSimpleName();
     private SeekBar seekBar;
     private TextView countResult;
+    private MyReceiver myReceiver = new MyReceiver();
+
+    private class MyReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int maxSeekBare = intent.getIntExtra("maxCountValue", -1);
+            int counterValue = intent.getIntExtra("counterValue", -1);
+            int setCounterZero = intent.getIntExtra("setCounterZero", -1);
+            if(maxSeekBare>0){
+                seekBar.setMax(maxSeekBare);
+            }
+            if(counterValue>-1){
+                seekBar.setProgress(counterValue);
+                countResult.setText(""+counterValue);
+            }
+            if(setCounterZero>-1){
+                seekBar.setProgress(setCounterZero);
+                countResult.setText(""+setCounterZero);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +45,42 @@ public class StartServiceActivity extends AppCompatActivity {
         initView();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter("suhun");
+        registerReceiver(myReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(myReceiver);
+    }
+
     private void initView(){
         seekBar = findViewById(R.id.lid_seekBar);
         countResult = findViewById(R.id.lid_counterResult);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    Intent intent = new Intent(StartServiceActivity.this, MyStartService.class);
+                    intent.putExtra("userChange", progress);
+                    startService(intent);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void startStartService(View view){
